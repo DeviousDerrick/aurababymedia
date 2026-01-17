e6a562f72a81b8073fa6ad162b3a28d2dc0c7593
DeviousDerrick <derrickmccabe0@gmail.com>
2026-01-17 06:31:15 -0600

    Implement movie fetching and display functionality
    Fetch popular movies from the API and display them.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
+const container = document.getElementById("movies");
 
+fetch("/api/popular")
+  .then(res => res.json())
+  .then(data => {
+    container.innerHTML = "";
+
+    data.results.forEach(movie => {
+      const div = document.createElement("div");
+      div.className = "movie";
+      div.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
+        <h3>${movie.title}</h3>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => {
+    container.textContent = "Failed to load movies";
+    console.error(err);
+  });
